699745723c0487a0e634710f42d5a9fb171d45fb
Veronica Cesari Carson <verocesari@gmail.com>
2022-01-28 21:18:28 -0300

    Proyecto
    Proyecto terminado

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Fragment, useState, useEffect} from 'react';
+import Header from './components/Header';
+import Formulario from './components/Formulario';
+import ListadoDeNoticias from './components/ListadoDeNoticias';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+  // definir la categoría y noticias
+
+  const [categoria, guardarCategoria] = useState('');
+
+  const [noticias, guardarNoticias] = useState([]);
+  // Una vez que el usuario selecciona una categoría y envía el formulario
+// tenemos que hacer las consultas para traernos esa categoría
+//importamos useEffect para que cuando detecte un cambio en categoría se vuelva
+// a ejecutar el componente
+
+  useEffect(() => {
+    const consultarAPI = async () => {
+      const url = `https://newsapi.org/v2/top-headlines?country=ar&category=${categoria}&apiKey=6028e0feb6da425aa9efccd8f751ca42`
+
+      const respuesta = await fetch(url);
+      const noticias = await respuesta.json();
+
+      guardarNoticias(noticias.articles); // cuando el usuario busque una categría y le de submit el state de noticias va a estar lleno
+    }
+
+    consultarAPI();
+  }, [categoria]);
+
+
+
+
+return (
+    <Fragment>
+      <Header 
+      titulo='Buscador de Noticias'
+      />
+
+      <div className="container white">
+        <Formulario 
+        guardarCategoria={guardarCategoria}
+        />
+        <ListadoDeNoticias
+           noticias={noticias}
+        />
+     
+      </div>
+    </Fragment>
   );
 }
 
---
+++ src/components/ListadoDeNoticias.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Noticia from './Noticia';
+import PropTypes from 'prop-types';
+
+const ListadoDeNoticias = ({noticias}) => (
+    
+
+        <div className="row">
+            {noticias.map(noticia => (
+                <Noticia
+                    key={noticia.url}
+                    noticia={noticia}
+                />
+            ))}
+
+        </div>
+    
+);
+
+ListadoDeNoticias.propTypes = {
+    noticias: PropTypes.array.isRequired
+}
+
+export default ListadoDeNoticias;
